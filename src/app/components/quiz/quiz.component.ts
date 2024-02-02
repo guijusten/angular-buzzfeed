f58a1b980cebee875609bc317d6604cdfc892897
Guilherme Fiorini <gui.justen@gmail.com>
2024-02-02 14:17:37 -0300

    adding html quiz structure

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+type Data = {
+  title: string;
+  questions: [];
+  results: {};
+};
+
+type Question = {
+  id: number;
+  question: string;
+  options: [];
+};
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit {
-  constructor() {}
+  data: Data = { title: '', questions: [], results: {} };
+  index: number = 0;
+  current_question: Question = { id: 0, question: '', options: [] };
+
+  constructor() {
+    var json = require('../../../assets/data/quiz-questions.json');
+    this.data = json;
+    this.current_question = this.data.questions[this.index];
+  }
 
   ngOnInit(): void {}
 }
