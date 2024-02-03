7c38cd28c2ceb7648fcb05c9ef8c1cd8c3a30944
Guilherme Fiorini <gui.justen@gmail.com>
2024-02-03 17:30:40 -0300

    add logic to quiz
    Options now can be clicked and its effect accounted for. At the end of
    the quiz, the result is displayed according to the users choices

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -12,6 +12,12 @@ type Question = {
   options: [];
 };
 
+type Option = {
+  id: number;
+  name: string;
+  alias: string;
+};
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
@@ -21,6 +27,30 @@ export class QuizComponent implements OnInit {
   data: Data = { title: '', questions: [], results: {} };
   index: number = 0;
   current_question: Question = { id: 0, question: '', options: [] };
+  user_choices: string[] = [];
+  isPlaying: boolean = true;
+  result: string = '';
+
+  countOccurences(element: string) {
+    return this.user_choices.reduce(
+      (count, current_elt) => (current_elt == element ? count + 1 : count),
+      0
+    );
+  }
+
+  handleClick = (prop: Option) => {
+    this.user_choices.push(prop.alias);
+
+    this.index += 1;
+    if (this.data.questions[this.index]) {
+      this.current_question = this.data.questions[this.index];
+    } else {
+      this.result = 'Herói';
+      if (this.countOccurences('A') > this.countOccurences('B'))
+        this.result = 'Vilão';
+      this.isPlaying = !this.isPlaying;
+    }
+  };
 
   constructor() {
     var json = require('../../../assets/data/quiz-questions.json');
