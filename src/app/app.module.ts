4514f2fb82ec1041e28463c72540a407a89a15ef
Guilherme Fiorini <gui.justen@gmail.com>
2024-02-04 20:35:03 -0300

    add footer
    Add footer that contains linkedin and github links.
    The footer is directly injected in the app.component.html file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, QuizComponent, HomeComponent],
+  declarations: [AppComponent, QuizComponent, HomeComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
